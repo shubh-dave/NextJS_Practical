82b97ac82e74dc731f3dd75091d201c511a31b47
shubh-dave <shubh.d@simformsolutions.com>
2022-09-19 17:53:08 +0530

    Image Tag NextJS

--- exercise-5/pages/index.tsx
+++ exercise-5/pages/index.tsx
@@ -1,11 +1,17 @@
 // ✍️ import the Image component from 'next/image'
-
+import Image from "next/image";
 // ✍️ import "lazar.png" statically
+import lazar from "../lazar.png";
+const Twitter_image =
+  "https://pbs.twimg.com/media/D2Nic0OWkAs3CF0?format=jpg&name=medium";
 
 const Home = () => {
-  return {
-    /* ✍️ add the lazar image here */
-  }
-}
+  return (
+    <>
+      <Image src={lazar} alt="Lazar" />
+      <Image src={Twitter_image} alt="Twitter_image" width={500} height={500} />
+    </>
+  );
+};
 
-export default Home
+export default Home;

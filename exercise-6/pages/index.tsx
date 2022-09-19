fea84a3ca6fe4142c43e0fbbaa1fe61daad76856
shubh-dave <shubh.d@simformsolutions.com>
2022-09-19 18:14:20 +0530

    Metadata practise

--- exercise-6/pages/index.tsx
+++ exercise-6/pages/index.tsx
@@ -1,14 +1,27 @@
 // ✍️ import Head from 'next/head'
-import { ChakraProvider, Heading } from '@chakra-ui/react'
+
+import Head from "next/head";
+import { ChakraProvider, Heading } from "@chakra-ui/react";
 
 const Home = () => {
   return (
     <ChakraProvider>
+      <Head>
+        <title>Homepage</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Head>
+        <title>Bye</title>
+        <meta name="NextPractise" content="Next Practise" key="title" />
+      </Head>
+      <Head>
+        <meta name="NextPractise" content="Best at next" key="title" />
+      </Head>
       {/* ✍️ add title and description using the Head component */}
       <Heading w="full" textAlign="center">
         This is our homepage content!
       </Heading>
     </ChakraProvider>
-  )
-}
-export default Home
+  );
+};
+export default Home;

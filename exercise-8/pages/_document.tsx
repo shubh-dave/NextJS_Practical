878eed3d169dc7ffafcbfc3b62d1cef258e2c61d
shubh-dave <shubh.d@simformsolutions.com>
2022-09-20 11:34:37 +0530

    _document file creation

---
+++ exercise-8/pages/_document.tsx
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from "next/document";
+const Document = () => {
+  return (
+    <Html lang="en">
+      <Head>
+        <title>Doing NextJS</title>
+        <link />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
+
+export default Document;
